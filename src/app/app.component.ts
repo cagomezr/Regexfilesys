e7df6068ad9e8343b929019b963d981018df25f6
arcanine300 <ikeellyou.dkh@gmail.com>
2019-12-04 15:09:38 -0500

    working on regex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'project3';
+export class AppComponent implements OnInit {
+  private title = 'project3';
+  private currentRegex: string = '';
+  private box1Txt: string = "filler";
+  private box2Txt: string = "filler";
+  private box3Txt: string = "filler";
+  private box4Txt: string = "filler";
+  private box5Txt: string = "filler";
+  private box6Txt: string = "filler";
+  private boxes: [boolean];
+
+  ngOnInit() {
+    
+  }
+
+  constructRegex(regex: string) {
+    this.currentRegex = regex;
+    for (let i = 0; i < 6; i++) {
+      
+    }
+  }
 }
