025bc0a91c8c43130cd56f72183be001001ea447
arcanine300 <ikeellyou.dkh@gmail.com>
2019-12-04 16:02:28 -0500

    linked html table values to ngModel and added event triggers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,43 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   private title = 'project3';
   private currentRegex: string = '';
-  private box1Txt: string = "filler";
-  private box2Txt: string = "filler";
-  private box3Txt: string = "filler";
-  private box4Txt: string = "filler";
-  private box5Txt: string = "filler";
-  private box6Txt: string = "filler";
-  private boxes: [boolean];
+  private boxes: file[] = [];
 
   ngOnInit() {
-    
+    for (let i = 0; i < 6; i++) {
+      let newFile = new file();
+      newFile.originalText = 'filler';
+      newFile.newText = newFile.originalText;
+      newFile.savedText = newFile.originalText;
+      newFile.selected = false;
+      this.boxes.push(newFile)
+      //console.log(this.boxes[i])
+    }
   }
 
   constructRegex(regex: string) {
     this.currentRegex = regex;
     for (let i = 0; i < 6; i++) {
-      
+      if (this.boxes[i].selected) {
+        this.boxes[i].newText = regex;
+      }
     }
   }
+
+  applyRegex(index: number) {
+    console.log(index)
+    if (this.boxes[index].selected) {
+      this.boxes[index].newText = this.currentRegex;
+    }else {
+      this.boxes[index].newText = this.boxes[index].originalText;
+    }
+  }
+
+}
+
+export class file {
+  originalText: string;
+  newText: string;
+  savedText: string;
+  selected: boolean;
 }
