bacf2d05916f099e299492e12e64f745da608a32
Roman Rezinkin <roman.rezinkin@hotmail.com>
2019-12-05 15:03:42 -0500

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,90 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
-  private title = 'project3';
+  private words = ["filler", "BigPhoto.png", "desktopBackground.jpeg", "photoshopFile.psd", "gimp.exe" , "Assignmen3.doc"];
+  private title = 'Assignment 3';
   private currentRegex: string = '';
   private boxes: file[] = [];
-
+  
   ngOnInit() {
     for (let i = 0; i < 6; i++) {
       let newFile = new file();
-      newFile.originalText = 'filler';
-      newFile.newText = newFile.originalText;
-      newFile.savedText = newFile.originalText;
+      newFile.originalText = this.words[i];
+      //newFile.savedText = newFile.originalText;
       newFile.selected = false;
-      this.boxes.push(newFile)
-      //console.log(this.boxes[i])
+      this.boxes.push(newFile);
+    }
+  }
+  // Rename Portion
+  applyRename(regex: string) {
+    const exp = new RegExp(regex);
+    if (regex) {
+      if (exp) {
+        for (let i = 0; i < 6; i++) {
+          if (this.boxes[i].selected) {
+            this.boxes[i].newText = this.boxes[i].originalText.replace(exp, this.boxes[i].originalText + " + sup");
+          }else {
+            this.boxes[i].newText = ""
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < 6; i++) {
+        if (this.boxes[i].selected) {
+          this.boxes[i].newText = "";
+        }
+      }
     }
   }
 
+  // Selection of files.
   constructRegex(regex: string) {
     this.currentRegex = regex;
-    const exp = new RegExp(this.currentRegex)
-
-    for (let i = 0; i < 6; i++) {
-      if (this.boxes[i].selected) {
-        this.boxes[i].newText.replace(exp, "1");
-        //this.boxes[i].newText = regex;
+    const exp = new RegExp(this.currentRegex);
+    if (regex) {
+      if (exp) {
+        for (let i = 0; i < 6; i++) {
+          if (this.boxes[i].originalText.match(exp)) {
+            this.boxes[i].selected = true;
+          } else {
+            this.boxes[i].selected = false;
+          }
+        }
+      }
+    } else {
+      for (let i = 0; i < 6; i++) {
+        if (this.boxes[i].selected) {
+          this.boxes[i].selected = false;
+          this.boxes[i].newText = "";
+        }
       }
     }
   }
-
+  
   applyRegex(index: number) {
-    console.log(index)
     if (this.boxes[index].selected) {
-      this.boxes[index].newText = this.currentRegex;
-    }else {
       this.boxes[index].newText = this.boxes[index].originalText;
+    }else {
+      this.boxes[index].newText = "";
     }
   }
 
+  // Save Button
   saveNames() {
     for (let i = 0; i < 6; i++) {
+      console.log(this.boxes[i].newText);
       if (this.boxes[i].selected) {
-        this.boxes[i].savedText = this.boxes[i].newText;
+        this.boxes[i].savedText =  this.boxes[i].newText;
       }
     }
   }
-
 }
 
 export class file {
